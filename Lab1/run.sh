0353dade3efdb92a58d229c7b3c16a6b135c9eb6
EEE4121F-B <Tristyn>
2023-04-25 15:39:26 +0200

    Added results

--- Lab1/run.sh
+++ Lab1/run.sh
@@ -1,3 +1,4 @@
+# Experiment 1
 echo "Starting congestion control algorithm experiments"
 sudo python tcp.py --bw-host 1000 --bw-net 2 --delay 50 --dir result/Reno --time 20 --maxq 100 --cong reno --qman pfifo_fast
 python plot_ping.py -f result/Reno/pingOut.txt -o result/pingReno.png
@@ -11,6 +12,8 @@ sudo python tcp.py --bw-host 1000 --bw-net 2 --delay 50 --dir result/BBR --time 
 python plot_ping.py -f result/BBR/pingOut.txt -o result/pingBBR.png
 python plot_queue.py -f result/BBR/q.txt -o result/qBBR.png
 echo "TCP BBR done"
+sleep 5 # sleep for 5s before starting next experiment
+#Experiment 2
 echo "Starting q size = 20 experiments"
 sudo python tcp.py --bw-host 1000 --bw-net 2 --delay 50 --dir result/Reno --time 20 --maxq 20 --cong reno --qman pfifo_fast
 python plot_ping.py -f result/Reno/pingOut.txt -o result/pingReno_q20.png
@@ -24,6 +27,8 @@ sudo python tcp.py --bw-host 1000 --bw-net 2 --delay 50 --dir result/BBR --time 
 python plot_ping.py -f result/BBR/pingOut.txt -o result/pingBBR_q20.png
 python plot_queue.py -f result/BBR/q.txt -o result/qBBR_q20.png
 echo "TCP BBR done"
+sleep 5 # sleep for 5s before starting next experiment
+# Experiment 3
 echo "Starting host speed = 100Mbps experiments"
 sudo python tcp.py --bw-host 100 --bw-net 2 --delay 50 --dir result/Reno --time 20 --maxq 100 --cong reno --qman pfifo_fast
 python plot_ping.py -f result/Reno/pingOut.txt -o result/pingReno_h100.png
